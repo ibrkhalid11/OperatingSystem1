ac76fecfa930e9314b248234d8b52f1d90dd9ce3
ibrkhalid11 <ibrkhalid11@gmail.com>
2025-12-17 21:54:59 -0700

    We have multiboot2 working. will start by handling text functionality and then moving to core os

--- MyOS/iso.sh
+++ MyOS/iso.sh
@@ -9,7 +9,7 @@ mkdir -p isodir/boot/grub
 cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "myos" {
-	multiboot /boot/myos.kernel
+	multiboot2 /boot/myos.kernel
 }
 EOF
 grub-mkrescue -o myos.iso isodir
