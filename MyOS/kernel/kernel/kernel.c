2cb1e200fb41f01f7993ec87aa3e2caa6bbec3db
ibrkhalid11 <ibrkhalid11@gmail.com>
2025-12-25 23:03:03 -0700

    implemented very basic GDT, will see more abt TSS

--- MyOS/kernel/kernel/kernel.c
+++ MyOS/kernel/kernel/kernel.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
-
+#include <kernel/gdt.h>
 void kernel_main(void){
     terminal_initialize();
+    initGdt();
+    printf("GDT is initialized \n");
     printf("hello Kernel World \n");
     printf("this is line 2\n");
     printf("this is line 3\n");
